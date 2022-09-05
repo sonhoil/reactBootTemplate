5805432a98d1846a3f479f5ce5f90742a55fde18
cooking <cooking@192.168.6.95>
2022-09-05 16:03:48 +0900

    first commit
    프로젝트 생성

---
+++ reactBootTemplate/src/main/java/com/reactboot/template/ReactBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.reactboot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBootTemplateApplication.class, args);
+	}
+
+}
---
+++ reactBootTemplate/src/test/java/com/reactboot/template/ReactBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactboot.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
