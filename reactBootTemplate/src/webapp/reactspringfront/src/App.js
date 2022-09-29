d03ece118dd9f17030ee4799b79b4d8571094229
hoil <som6642@naver.com>
2022-09-29 21:31:52 +0900

    React 활용 - 2 : React-router-dom(네비게이션) 특정 페이지에서 숨기기

--- reactBootTemplate/src/webapp/reactspringfront/src/App.js
+++ reactBootTemplate/src/webapp/reactspringfront/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import FirstScreen from './Screens/FirstScreen'
+import HeaderHideScreen from './Screens/HeaderHideScreen'
+import Header from './Screens/Header'
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,10 +13,11 @@ function App() {
   return (
     <Router>
           {/* 실제 화면에 보여지는 태그, 클릭 시 주소창이 /FirstScreen으로 이동 */}
-          <Link to="/FirstScreen">FirstScreen</Link>
+          <Header/>
           {/* 주소창 변함에 따라 화면을 해당 주소창에 맞게 변경해준다. */}
           <Routes>
             <Route path="/FirstScreen"  element={<FirstScreen />}></Route>
+            <Route path="/HeaderHideScreen"  element={<HeaderHideScreen />}></Route>
           </Routes>
     </Router>
   );
